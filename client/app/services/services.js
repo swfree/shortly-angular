dc53afd4677d2513785e5a1439a7ecbaf3689249
swfree <skye.free@gmail.com>
2016-04-01 17:36:10 -0700

    Pass all tests, and partially implement view rendering

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -2,6 +2,33 @@ angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
   // Your code here
+  var getAll = function () {
+    return $http({
+      method: 'GET',
+      url: '/api/links'
+    })
+    .then(function (resp) {
+      return resp.data;
+    });
+  };
+
+  var addOne = function (newLink) {
+    console.log('------Links factory new link: ', newLink);
+    return $http({
+      method: 'POST',
+      url: '/api/links',
+      data: newLink
+    })
+    .then(function (resp) {
+      return resp;
+    });
+  };
+
+  return {
+    getAll: getAll,
+    addOne: addOne
+  };
+
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,4 +2,10 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+  $scope.link;
+
+  $scope.addLink = function() {
+    console.log("-------shorten ctrl link: ", $scope.link);
+    Links.addOne($scope.link);
+  };
 });
